b19c917edb3d33a7ccc98b0261fc8dce3dc65c25
Estevamrod <estevamot@gmail.com>
2024-11-11 17:20:01 -0300

    Solve problem with import

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import * as React from 'react'
+import {useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Tiles from '../components/pages/Game/Tiles';
 import ScoreBar from '../components/pages/Game/ScoreBar';
